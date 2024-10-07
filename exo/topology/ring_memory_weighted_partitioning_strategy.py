1a4edca0838e96d16515ebfbe1b7f819d1d1534d
Daniel Newman <me@dtnewman.com>
2024-10-07 14:19:55 -0400

    Fix sorting issue and last partition issue in RingMemoryWeightedPartitioningStrategy + fix tests

--- exo/topology/ring_memory_weighted_partitioning_strategy.py
+++ exo/topology/ring_memory_weighted_partitioning_strategy.py
@@ -7,12 +7,12 @@ from .partitioning_strategy import Partition
 class RingMemoryWeightedPartitioningStrategy(PartitioningStrategy):
   def partition(self, topology: Topology) -> List[Partition]:
     nodes = list(topology.all_nodes())
-    nodes.sort(key=lambda x: (x[1].memory, x[0]), reverse=True)
+    nodes.sort(key=lambda x: (-x[1].memory, x[0]))  # Sort by memory in descending order, then by node ID
     total_memory = sum(node[1].memory for node in nodes)
     partitions = []
     start = 0
-    for node in nodes:
-      end = round(start + (node[1].memory/total_memory), 5)
+    for i, node in enumerate(nodes):
+      end = 1.0 if i == len(nodes) - 1 else round(start + (node[1].memory/total_memory), 5)
       partitions.append(Partition(node[0], start, end))
       start = end
     return partitions
