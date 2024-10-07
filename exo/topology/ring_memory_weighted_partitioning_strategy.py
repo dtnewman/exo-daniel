8b4a8c17af77f769cd584b26e6501e6865f36f73
Daniel Newman <me@dtnewman.com>
2024-10-07 17:40:12 -0400

    foo

--- exo/topology/ring_memory_weighted_partitioning_strategy.py
+++ exo/topology/ring_memory_weighted_partitioning_strategy.py
@@ -7,7 +7,7 @@ from .partitioning_strategy import Partition
 class RingMemoryWeightedPartitioningStrategy(PartitioningStrategy):
   def partition(self, topology: Topology) -> List[Partition]:
     nodes = list(topology.all_nodes())
-    nodes.sort(key=lambda x: (-x[1].memory, x[0]))  # Sort by memory in descending order, then by node ID
+    nodes.sort(key=lambda x: (-x[1].memory, -x[0]))  # Sort by memory in descending order, then by node ID
     print('nodes:')
     for node in nodes:
       print(f"{node[0]}: {node[1].memory}")
@@ -18,7 +18,7 @@ class RingMemoryWeightedPartitioningStrategy(PartitioningStrategy):
       end = 1.0 if i == len(nodes) - 1 else round(start + (node[1].memory/total_memory), 5)
       partitions.append(Partition(node[0], start, end))
       start = end
-    print('partitions:')
-    for partition in partitions:
-      print(f"{partition.node_id}: {partition.start} - {partition.end}")
+    # print('partitions:')
+    # for partition in partitions:
+    #   print(f"{partition.node_id}: {partition.start} - {partition.end}")
     return partitions
