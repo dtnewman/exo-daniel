647f9b90f00eec9b122316fe3bb3c11ecac4b596
Daniel Newman <me@dtnewman.com>
2024-10-08 14:50:09 -0400

    foo

--- exo/orchestration/standard_node.py
+++ exo/orchestration/standard_node.py
@@ -242,7 +242,7 @@ class StandardNode(Node):
     shard = self.get_current_shard(base_shard)
 
     partitions = self.partitioning_strategy.partition(self.topology)
-    print(f"Partitions: {partitions}")
+    print(f"\n\nPartitions: {partitions}\n\n")
     shards = map_partitions_to_shards(self.partitioning_strategy.partition(self.topology), base_shard.n_layers, base_shard.model_id)
     current_partition_index = next((i for i, p in enumerate(partitions) if p.node_id == self.id), None)
     if DEBUG >= 1: print(f"Current partition index: {current_partition_index}")
@@ -251,7 +251,7 @@ class StandardNode(Node):
       next_partition: Partition = partitions[next_partition_index]
       next_shard = shards[next_partition_index]
       # if DEBUG >= 2: print(f"Computed next from: {shard}, {self.topology}. Next partition: {next_partition}")
-      print(f"Computed next from: {shard}, {self.topology}. Next partition: {next_partition}")
+      print(f"\n\nComputed next from: {shard}, {self.topology}. Next partition: {next_partition}\n\n")
 
       if next_partition.node_id == self.id:
         if isinstance(tensor_or_prompt, np.ndarray):
