0cd58700ab9c726a88ab43e4764e96e4bfccbdbc
Daniel Newman <me@dtnewman.com>
2024-10-08 12:11:58 -0400

    small updates to mock inference engine

--- exo/inference/mock_engine/inference.py
+++ exo/inference/mock_engine/inference.py
@@ -1,15 +1,15 @@
 import json
 from exo.inference.shard import Shard
 from exo.inference.inference_engine import InferenceEngine
-from typing import Optional, Tuple
+from typing import List, Optional, Tuple
 import numpy as np
 
 
 class MockInferenceEngine(InferenceEngine):
     
-    def __init__(self):
-        self.response = iter([2646, 16926, 1095, 499, 1523, 128009])
-        self.input_data = [51585, 65267, 53498, 52743, 63954, 61797, 56691, 61758, 60298, 62148, 59485, 57423, 61918, 57683, 49417, 61158, 62351, 52365, 52251]
+    def __init__(self, input_data: List[int], response: List[int]):
+        self.input_data = input_data or [51585, 65267, 53498, 52743, 63954, 61797, 56691, 61758, 60298, 62148, 59485, 57423, 61918, 57683, 49417, 61158, 62351, 52365, 52251]
+        self.response = iter(response or [2646, 16926, 1095, 499, 1523, 128009])
 
     async def infer_prompt(self, request_id: str, shard: Shard, prompt: str, image_str: Optional[str] = None, inference_state: Optional[str] = None) -> Tuple[np.ndarray, str, bool]:
         array = self.input_data
@@ -22,5 +22,6 @@ class MockInferenceEngine(InferenceEngine):
         response = np.array([next_token]), json.dumps({"cached_iids": cached_iids}), is_finished
         return response
 
-# Dynamically modify the __name__ attribute of the class so that it acts like the pytorch inference engine
+# Dynamically modify the __name__ attribute of the class so that it acts like the pytorch inference engine.
+# This is necessary so that it can pick up settings related to the pytorch inference engine (e.g., eligible models).
 MockInferenceEngine.__name__ = "PyTorchDynamicShardInferenceEngine"
