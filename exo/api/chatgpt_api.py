2364d603deb2bde28a75ae23b2a9eac72f193a09
Nel Nibcord <blindcrone@tuta.io>
2024-11-12 07:33:15 -0800

    Just wanna see

--- exo/api/chatgpt_api.py
+++ exo/api/chatgpt_api.py
@@ -201,7 +201,7 @@ class ChatGPTAPI:
     return web.FileResponse(self.static_dir/"index.html")
 
   async def handle_model_support(self, request):
-    return web.json_response({"engine pool": list(dict.fromkeys([inference_engine_classes.get(i,None) for i in self.node.topology_inference_engines_pool for i in i if i is not None] + [self.inference_engine_classname]))})
+    return web.json_response({"model pool": [k for k in model_cards.keys() if all(list(dict.fromkeys([inference_engine_classes.get(i,None) for i in self.node.topology_inference_engines_pool for i in i if i is not None] + [self.inference_engine_classname])) in model_cards["repo"])})
   
   async def handle_get_models(self, request):
     return web.json_response([{"id": model_name, "object": "model", "owned_by": "exo", "ready": True} for model_name, _ in model_cards.items()])
