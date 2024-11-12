f917e976dc7530aa77a9e28cb5e474a11814a8c4
Nel Nibcord <blindcrone@tuta.io>
2024-11-12 07:29:03 -0800

    Just wanna see

--- exo/inference/inference_engine.py
+++ exo/inference/inference_engine.py
@@ -5,6 +5,8 @@ from exo.helpers import DEBUG  # Make sure to import DEBUG
 from typing import Tuple, Optional
 from abc import ABC, abstractmethod
 from .shard import Shard
+
+
 class InferenceEngine(ABC):
   @abstractmethod
   async def encode(self, shard: Shard, prompt: str) -> np.ndarray:
@@ -27,24 +29,26 @@ class InferenceEngine(ABC):
     output_data = await self.infer_tensor(request_id, shard, tokens, inference_state)
     return output_data 
 
-
-def inference_engine_classes():
-  from exo.inference.tinygrad.inference import TinygradDynamicShardInferenceEngine
-  from exo.inference.mlx.sharded_inference_engine import MLXDynamicShardInferenceEngine
-  from exo.inference.dummy_inference_engine import DummyInferenceEngine
-  return {
-    "mlx": MLXDynamicShardInferenceEngine,
-    "tinygrad": TinygradDynamicShardInferenceEngine,
-    "dummy": DummyInferenceEngine,
-  }
+inference_engine_classes = {
+  "mlx": "MLXDynamicShardInferenceEngine",
+  "tinygrad": "TinygradDynamicShardInferenceEngine",
+  "dummy": "DummyInferenceEngine",
+}
 
 def get_inference_engine(inference_engine_name: str, shard_downloader: 'ShardDownloader'):
   if DEBUG >= 2:
     print(f"get_inference_engine called with: {inference_engine_name}")
-  if inference_engine_name == "tinygrad":
+  if inference_engine_name == "mlx":
+    from exo.inference.mlx.sharded_inference_engine import MLXDynamicShardInferenceEngine
+
+    return MLXDynamicShardInferenceEngine(shard_downloader)
+  elif inference_engine_name == "tinygrad":
+    from exo.inference.tinygrad.inference import TinygradDynamicShardInferenceEngine
     import tinygrad.helpers
     tinygrad.helpers.DEBUG.value = int(os.getenv("TINYGRAD_DEBUG", default="0"))
-  classes = inference_engine_classes()
-  if inference_engine_name in classes:
-    return classes[inference_engine_name](shard_downloader)
+
+    return TinygradDynamicShardInferenceEngine(shard_downloader)
+  elif inference_engine_name == "dummy":
+    from exo.inference.dummy_inference_engine import DummyInferenceEngine
+    return DummyInferenceEngine()
   raise ValueError(f"Unsupported inference engine: {inference_engine_name}")
